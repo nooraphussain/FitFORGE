ad5d2c4b3befc36d12283920bc30a6119e91c56e
nooraphussain <noorahussainp2002@gmail.com>
2025-04-18 10:59:57 +0530

    commiting all the utils

---
+++ utils/categoryMessages.js
@@ -0,0 +1,10 @@
+const CATEGORY_MESSAGES = Object.freeze({
+    CATEGORY_ALREADY_EXISTS: 'Category already exists',
+    CATEGORY_ADDED_SUCCESSFULLY: 'Category added successfully',
+    VALIDATION_DESCRIPTION_REQUIRED: 'Category validation failed: `description` is required.',
+    CATEGORY_NOT_FOUND: 'Category not found!',
+    INTERNAL_SERVER_ERROR: 'Internal Server error'
+  });
+  
+module.exports = CATEGORY_MESSAGES;
+  
